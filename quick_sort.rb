c980bc2ac644caf3d74e1c516c4254fc3a19d2c3
Joshua Wolfe <jlwolfe1990@gmail.com>
2016-01-27 02:09:31 -0600

    Ensure more complex input passes

--- quick_sort.rb
+++ quick_sort.rb
@@ -32,6 +32,10 @@ end
 
 class QuickSortTest < Minitest::Test
   def test_that_the_output_is_correct_for_a_more_complex_case
-    assert_equal "#1A - Kessenich\n#1B - Adams\n#8 - Johnson\n#10A - Wolfe\n#50 - Smith\n#100 - Sanders", QuickSort.perform("sample_input.txt")
+    assert_equal "#1 - Kessenich\n#1A - Kessenich\n#1B - Adams\n#8 - Johnson\n#10A - Wolfe\n#50 - Smith\n#100 - Sanders", QuickSort.perform("sample_input.txt")
+  end
+
+  def test_that_the_output_is_correct_for_the_basic_example
+    assert_equal "#1A - Kessenich\n#1B - Adams\n#8 - Johnson\n#50 - Smith\n#100 - Sanders", QuickSort.perform("basic_input.txt")
   end
 end
